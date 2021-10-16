cd65c856ab16e348ca7c295785c8f957c3ddb80b
ampo0809 <ampuero0809@gmail.com>
2021-10-16 14:07:25 +0200

    Added the 10000 word list into the game

--- jumper/game/generator.py
+++ jumper/game/generator.py
@@ -22,26 +22,31 @@ class Generator:
     self.word_letters = []
 
   def read_external_file(self, filename):
+    # Reads an external file and imports it as a list of words. This is very standard python code from CSE 111
     word_list = []
     with open(filename, "rt") as text_file:
         for line in text_file:
             clean_line = line.strip()
             word_list.append(clean_line)
-
-    print(word_list)
+    # print(word_list)
     return word_list
 
   def import_words(self):
-    # try:
-      self.read_external_file("wordlist10000.txt")
-    # except (FileNotFoundError, PermissionError) as error:
-    #     print(type(error).__name__, error, sep=": ")
+    try:
+      # Tries to import the wordlist of 10000 entries from the external file. It it success, it will replace the basic `self.words` list
+      impoted_words = self.read_external_file("game/wordlist10000.txt")
+      # Only allows words with 4 or more characters into the game
+      words_filtered_by_length = [word for word in impoted_words if len(word) >= 4]
+      self.words = words_filtered_by_length
+      # If this fails, the game will continue with the basic `self.words` list 
+    except (FileNotFoundError, PermissionError) as error:
+        print(type(error).__name__, error, sep=": ")
 
   def generate_word(self):
-    # Generates the word random
-
+    # By calling this method, the basic 3 word list gets replaced with thousands of words from the external file
     self.import_words()
 
+    # Generates the word random
     word_index = random.randint(1, len(self.words))
     self.current_word = self.words[word_index - 1]
     self.split_word()
@@ -56,6 +61,7 @@ class Generator:
       self.word_letters.append(word[i])
 
   def check_characters(self, character) -> bool:
+    # Simply acts as a filter to check if the character entered by the player is an actual letter in the English alphabet
     alphabet_string = string.ascii_lowercase
     alphabet_list = list(alphabet_string)
 
